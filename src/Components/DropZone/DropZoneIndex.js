4df1aa2250a0b11baa6d3782af3216277193f090
Thiago <thiago.coelho158@gmail.com>
2022-05-26 12:13:57 -0300

    Fix drag and drop bugs

--- src/Components/DropZone/DropZoneIndex.js
+++ src/Components/DropZone/DropZoneIndex.js
@@ -2,34 +2,34 @@ import React, {useContext} from 'react'
 import './DropZoneStyle.css'
 
 export default function DropZoneIndex({ onDrop, formats }) {
-    const drop = React.useRef(null)
-    const [dragging, setDragging] = React.useState(false);
+  const drop = React.useRef(null)
+  const [dragging, setDragging] = React.useState(false);
 
   React.useEffect(() => {
     drop.current.addEventListener('dragover', handleDragOver);
     drop.current.addEventListener('dragenter', handleDragEnter);
     drop.current.addEventListener('dragleave', handleDragLeave);
     drop.current.addEventListener('drop', handleDrop);
-
+    
     return () => {
       drop.current.removeEventListener('dragover', handleDragOver);
       drop.current.removeEventListener('drop', handleDrop);
       drop.current.removeEventListener('dragenter', handleDragEnter);
       drop.current.removeEventListener('dragleave', handleDragLeave);
     };
-  }, []);
+  }, [drop, onDrop]);
 
 
   function handleDragEnter(e) {
     e.preventDefault();
     e.stopPropagation();
-        setDragging(true);
+    setDragging(true);
   };
   
   function handleDragLeave(e) {
     e.preventDefault();
     e.stopPropagation();
-        setDragging(false);
+    setDragging(false);
   };
 
   function handleDragOver(e) {
@@ -41,16 +41,16 @@ export default function DropZoneIndex({ onDrop, formats }) {
   function handleDrop(e) {
     onDrop(e);
     setDragging(false);
-    }
-
+  }
+ 
   return (
     <div className='DropZone' ref={drop}>
       {dragging ?
         <span className='DragOver'>
-                Solte aqui.
+          Solte aqui.
         </span> :
         <span>
-                 Arraste para aqui.
+          Arraste para aqui.
         </span>
       }
     </div>
