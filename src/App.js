c20f0fa38346a3468ccb8c750adb8a8871ec4b6c
Thiago <thiago.coelho158@gmail.com>
2022-05-26 12:12:56 -0300

    Reimplement dropped files handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import './App.css';
-import React from 'react'
-import DropBoxIndex from './Components/DropBox2/DropBoxIndex';
+import React, { useState } from 'react'
+import DropZoneIndex from './Components/DropZone/DropZoneIndex';
 import SvgGridIndex from './Components/SvgGrid/SvgGridIndex';
 
-export const DiagramContext = React.createContext()
 
 function App() {
+  const [files, setFiles] = useState([]);
+  
+  function readFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsText(file);
+    });
+  }
+
   async function handleDrop(e) {
     e.preventDefault();
     e.stopPropagation();
+    
+    const droppedFiles = e.dataTransfer.files;
+    const droppedFilesContent = [];
 
+    for (let i in droppedFiles) {
+      let item = droppedFiles[i];
+      if (typeof item === 'object') {
+        const fileContent = await readFile(item);
+        droppedFilesContent.push(fileContent);
+      }
+    }
+    
+    setFiles([...files, ...droppedFilesContent]);
   }
 
   return (
-    <DiagramContext.Provider value={diagram}>
-      <div className="App">
-        <DropBoxIndex setDiagram={setDiagram}/>
-      </div>
-    </DiagramContext.Provider>
+    <div className="App">
+      <DropZoneIndex onDrop={ handleDrop }/>
+      <SvgGridIndex files={ files }/>
+    </div>
   );
 }
 
